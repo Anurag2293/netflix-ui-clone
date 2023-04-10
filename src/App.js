96c326554742b5f2b23e2f4bf4dd826d7fb0698f
Anurag Dhote <anuragdhote392@gmail.com>
2023-04-10 20:10:02 +0530

    Titles basic finish

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css'
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import Billboard from './components/Billboard';
+import Titles from './components/Titles'
 
 function App() {
 	return (
 		<div className='App'>
 			<Billboard />
 			<Navigation />
+			<Titles />
 			<Footer />
 		</div>
 	);
