db7ea76d538e0f41bd03b381a3d394f724d8410e
Anurag Dhote <anuragdhote392@gmail.com>
2023-04-10 19:58:09 +0530

    Billboard Complete

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css'
 
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import Billboard from './components/Billboard';
 
 function App() {
 	return (
 		<div className='App'>
+			<Billboard />
 			<Navigation />
 			<Footer />
 		</div>
