629a6dda3991796e35b22d31528271d1b9026607
Anurag Dhote <anuragdhote392@gmail.com>
2023-04-10 19:29:33 +0530

    Footer Complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './normalize.css'
 import './App.css'
 
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 function App() {
 	return (
 		<div className='App'>
 			<Navigation />
+			<Footer />
 		</div>
 	);
 }
